d0d08647971fb3c67234d6be78c9122b8ce2819c
guo taian <18302010041@fudan.edu.cn>
2020-12-04 20:40:43 +0800

    commit

--- flaskr/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-from flask import Flask
-
-def hello():
-    print("hello")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lenovo\PycharmProjects\新建文件夹\DataBaseDesignProject\venv
+set VIRTUAL_ENV=C:\Users\Lenovo\PycharmProjects\flaskProject1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
