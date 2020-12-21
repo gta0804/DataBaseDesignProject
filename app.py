4e1e84a1dcb40a27d7644a052157b3933e4ad46b
guo taian <18302010041@fudan.edu.cn>
2020-12-21 19:20:46 +0800

    commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -8,5 +10,18 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/user/showAll")
+def show_all():
+    patient1 = {'name': '张三','gender': '男','age': 26,'address': '上海市虹口区松花江路2500号', 'temperature': '36.5'}
+    patient2 = {'name': '李四','gender': '女','age': 29,'address': '上海市虹口区松花江路2500号', 'temperature': '38.9'}
+    patients = [patient1,patient2]
+    role = 'doctor'
+    name = '李四'
+    return render_template("all_patient.html", patients=patients, role=role, name=name)
+
+
+@app.route("/doctor/nav")
+def doctor_nav():
+    role = 'doctor'
+    name = '李四'
+    return render_template("all_patient.html", role=role, name=name)
