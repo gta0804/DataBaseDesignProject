d7bc2a3323c38bebf244f14534dd4c592251489b
xiaoyang <1217456181@qq.com>
2021-01-02 13:35:03 +0800

    list new patient

--- emergency_nurse.py
+++ emergency_nurse.py
@@ -1,12 +1,13 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from models import Staff
-from entry import db
+from auth import session
 
 bp = Blueprint('emergency_nurse', __name__, url_prefix='/emergency_nurse')
 
-@bp.route('/new_patient', methods=('GET', 'POST'))
 
+@bp.route('/new_patient', methods=('GET', 'POST'))
 def new_patient():
-    return render_template("emergency_nurse/new_patient.html", role='emergency_nurse',name='12345')
+    return render_template("emergency_nurse/new_patient.html",
+                           username=session['user_name'], role=session['role'],
+                           area=session['area'])
